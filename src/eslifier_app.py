cad1492a797282da54e700c0cc6fc3fe60dc6e3f
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2026-04-27 02:31:28 -0500

    Update eslifier_app.py

--- src/eslifier_app.py
+++ src/eslifier_app.py
@@ -14,7 +14,7 @@ from settings_page import settings
 from main_page import main
 from log_stream import log_stream
 
-CURRENT_VERSION = '0.0.3'
+CURRENT_VERSION = '0.0.4'
 MAJOR, MINOR, PATCH = [int(x, 10) for x in CURRENT_VERSION.split('.')] 
 VERSION_TUPLE = (MAJOR, MINOR, PATCH)
 
